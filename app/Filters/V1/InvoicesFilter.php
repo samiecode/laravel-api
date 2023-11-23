71fd623b8cfa92ec53d6611eb5de85c8ad35343a
samiecode <samiekeyz02@gmail.com>
2023-11-23 16:39:46 +0100

    Laravel Api

---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filters\V1;
+
+use App\Filters\ApiFilter;
+
+class InvoicesFilter extends ApiFilter
+{
+
+    /**
+     * Summary of safeParams
+     * @var array
+     */
+    protected $safeParams = [
+        'customerId' => ['eq'],
+        'amount' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+        'status' => ['eq' , 'ne'],
+        'billedDate' => ['eq' , 'lt', 'lte', 'gt', 'gte'],
+        'paidDate' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+    ];
+
+    protected $columnMap = [
+        'customerId' => 'customer_id',
+        'billedDate' => 'billed_date',
+        'paidDate' => 'paid_date'
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+        'ne' => '!='
+    ];
+}
