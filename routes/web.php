a1f7d004d96d64e43a058ed0385d8f3a272a5257
samiecode <samiekeyz02@gmail.com>
2023-11-23 16:39:46 +0100

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup', function () {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if(!Auth::attempt($credentials)){
+        $user = new User([
+            'name' => 'Admin',
+            'email' => $credentials['email'],
+            'password' => Hash::make($credentials['password'])
+        ]);
+
+        $user->save();
+
+        if(Auth::attempt($credentials)){
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+            $updateToken = $user->createToken('update-token', ['create', 'update']);
+            $basicToken = $user->createToken('basic-token');
+        }
+
+        return [
+            'admin' => $adminToken->plainTextToken,
+            'update' => $updateToken->plainTextToken,
+            'basic' => $basicToken->plainTextToken
+        ];
+    }
 });
